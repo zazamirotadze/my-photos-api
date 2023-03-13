f28c4f195953b21d1c830e7ce267a2df1c0e7eea
zaza mirotadze <mirotadzezaza1@gmail.com>
2023-03-13 14:00:17 +0400

    first try of photos api 7

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 module.exports = [
-    {id: "snow-like-a-canyon", name: "Snow like a canyon", url:"https://iili.io/HXMCvbp.md.jpg" },
-    {id: "mountain-as-clouds-barrier", name: "Mountain as clouds barrier", url:"https://iili.io/HXMCzOl.md.jpg" },
-    {id: "algae?", name: "Algae?", url: "https://iili.io/HXMFfI9.md.jpg" }
+    {id: "snow-like-a-canyon", name: "Snow like a canyon", nameGeo: "თოვლი მზგავსად კანიონი", url:"https://iili.io/HXMCvbp.md.jpg" },
+    {id: "mountain-as-clouds-barrier", name: "Mountain as clouds barrier",  nameGeo: "მთა როგორც ღრუბლების ბარიერი", url:"https://iili.io/HXMCzOl.md.jpg" },
+    {id: "algae?", name: "Algae?", nameGeo: "წყალმცენარე?", url: "https://iili.io/HXMFfI9.md.jpg" }
 ]
 
