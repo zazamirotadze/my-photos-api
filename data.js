363b3ade2ff438489645a532a2d3c815be80a7ba
zaza mirotadze <mirotadzezaza1@gmail.com>
2023-03-13 16:27:11 +0400

    first try of photos api 7

--- data.js
+++ data.js
@@ -1,6 +1,12 @@
 module.exports = [
-    {id: "snow-like-a-canyon", name: "Snow like a canyon", nameGeo: "თოვლი მზგავსად კანიონი", url:"https://iili.io/HXMCvbp.md.jpg" },
-    {id: "mountain-as-clouds-barrier", name: "Mountain as clouds barrier",  nameGeo: "მთა როგორც ღრუბლების ბარიერი", url:"https://iili.io/HXMCzOl.md.jpg" },
-    {id: "algae?", name: "Algae?", nameGeo: "წყალმცენარე?", url: "https://iili.io/HXMFfI9.md.jpg" }
+    {id: "snow-like-a-canyon", name: "The Snow like a canyon", nameGeo: "თოვლი მზგავსად კანიონისა", url:"https://iili.io/HXMCvbp.md.jpg" },
+    {id: "mountain-as-clouds-barrier", name: "The Mountain as clouds barrier",  nameGeo: "მთა როგორც ღრუბლების ბარიერი", url:"https://iili.io/HXMCzOl.md.jpg" },
+    {id: "algae?", name: "A Algae?", nameGeo: "წყალმცენარე?", url: "https://iili.io/HXMFfI9.md.jpg" },
+    {id: "puppy", name: "A Puppy", nameGeo: "ლეკვი", url: "https://iili.io/HXMrEyN.jpg" },
+    {id: "spider-and-yellow-flower", name: "A Spider and a yellow flower", nameGeo: "ობობა და ყვითელი ყვავილი", url: "https://iili.io/HXMrRZF.jpg" },
+    {id: "cockroach1", name: "a cockroach", nameGeo: "ხოჭო", url: "https://iili.io/HXMruTP.jpg" },
+    {id: "black-spider", name: "A black spider", nameGeo: "შავი ობობა", url: "https://iili.io/HXMro3x.jpg" },
+    {id: "ragweed", name: "A ragweed", nameGeo: "ამბროზია", url: "https://iili.io/HXMrJv2.jpg" },
+    {id: "moonrise", name: "Moonrise", nameGeo: "მთვარის ამოსვლა", url: "https://iili.io/HXMrHYl.md.jpg" },
 ]
 
