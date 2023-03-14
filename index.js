3425fef49c75712f44736a753b28d2183b8bc79e
zaza mirotadze <mirotadzezaza1@gmail.com>
2023-03-14 20:33:06 +0400

    first try of photos api 18

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const corsMiddleware = require('./corsMiddleware');
 const app = express();
-const {readdirSync} = require("fs");
+const {readdirSync, readFileSync} = require("fs");
+const marked = require('marked');
+
 
 app.use(corsMiddleware);
+
 readdirSync("./routes").map((file) => app.use("/", require("./routes/" + file)))
 
+const readme = readFileSync('readme.md', 'utf8');
+
 app.use('/', (req, res) => {
-    res.send('My photos api')
+    res.send(marked.parse(readme))
 })
+
 app.listen(3000)
